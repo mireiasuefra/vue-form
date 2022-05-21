44aff8049df7d5cd0e82ec4a3a9be2155ccb9bcd
mireiasuefra <mireia.suefra@gmail.com>
2022-05-21 14:18:25 +0200

    add  styles

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from "vue-router";
     <img
       alt="Vue logo"
       class="logo"
-      src="@/assets/logoMS.png"
+      src="@/assets/logomireiasuero.png"
       width="125"
       height="125"
     />
